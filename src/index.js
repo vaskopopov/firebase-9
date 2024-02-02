f7dc6793dc1520bc7c7544e6362583edf0fcc6f0
Vasko <vaskopopov@gmail.com>
2024-02-02 23:38:08 +0100

    lesson 5 - Adding & Deleting Documents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import {
-  getFirestore, collection, getDocs
+  getFirestore, collection, getDocs,
+  addDoc, deleteDoc, doc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -32,4 +33,33 @@ const firebaseConfig = {
     })
     .catch(err => {
       console.log(err.message)
+    })
+
+    // adding documents
+    const addBookForm = document.querySelector('.add')
+    addBookForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value
+      })
+      .then(() => {
+        addBookForm.reset()
+      })
+
+    })
+
+
+    // deleting documents
+    const deleteBookForm = document.querySelector('.delete')
+    deleteBookForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+      deleteDoc(docRef)
+        .then(() => {
+          deleteBookForm.reset()
+        })
     })
