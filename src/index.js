99995915b441d9d0fa6dba13b78b3d60f51bceb0
Vasko <vaskopopov@gmail.com>
2024-02-02 22:33:06 +0100

    firebase install

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuKBBHaycfH79LRLdyyMrZuUwTZxEVBWI",
+    authDomain: "fir-9-dojo-2676e.firebaseapp.com",
+    projectId: "fir-9-dojo-2676e",
+    storageBucket: "fir-9-dojo-2676e.appspot.com",
+    messagingSenderId: "181856333130",
+    appId: "1:181856333130:web:315bbedace6f32bb8b25b3"
+  };
+
+  initializeApp(firebaseConfig)
