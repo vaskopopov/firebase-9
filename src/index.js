8a0d5f000961335d0ecaceae40dd59ec5143a1ec
Vasko <vaskopopov@gmail.com>
2024-02-02 23:55:55 +0100

    lesson 6 - Real time collection data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import {
-  getFirestore, collection, getDocs,
+  getFirestore, collection, onSnapshot,
   addDoc, deleteDoc, doc
 } from 'firebase/firestore'
 
@@ -22,33 +22,29 @@ const firebaseConfig = {
   // collection ref
   const colRef = collection(db, 'books')
 
-  // get collection data
-  getDocs(colRef)
-    .then((snapshot) => {
-      let books = []
-      snapshot.docs.forEach((doc) => {
-        books.push({ ...doc.data(), id: doc.id})
-      })
-      console.log(books)
+  // real time  collection data
+  onSnapshot(colRef, (snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id})
     })
-    .catch(err => {
-      console.log(err.message)
+    console.log(books)
+  })
+
+  // adding documents
+  const addBookForm = document.querySelector('.add')
+  addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+      title: addBookForm.title.value,
+      author: addBookForm.author.value
+    })
+    .then(() => {
+      addBookForm.reset()
     })
 
-    // adding documents
-    const addBookForm = document.querySelector('.add')
-    addBookForm.addEventListener('submit', (e) => {
-      e.preventDefault()
-
-      addDoc(colRef, {
-        title: addBookForm.title.value,
-        author: addBookForm.author.value
-      })
-      .then(() => {
-        addBookForm.reset()
-      })
-
-    })
+  })
 
 
     // deleting documents
