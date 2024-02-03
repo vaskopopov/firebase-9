432a0050ee2d54347b4baccafa6116057af80d5b
Vasko <vaskopopov@gmail.com>
2024-02-03 22:32:44 +0100

    #13 - Logging In & Out

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {
 
 import {
   getAuth,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signOut, signInWithEmailAndPassword
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -109,6 +110,34 @@ signupForm.addEventListener('submit', (e) => {
     .then((cred) => {
       console.log('use created: ', cred.user)
       signupForm.reset()
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('The user signed out')
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log('User logged in: ', cred.user)
     })
     .catch((err) => {
       console.log(err.message)
